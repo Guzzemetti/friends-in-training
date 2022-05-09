03c9c5acb68600da66b2a17749796c837f41a892
John Hysong <johnahysong@gmail.com>
2022-05-09 11:33:20 -0400

    worked on User, Gym, and index models. Updated text file with gym names

---
+++ models/Gym.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Gym extends Model {}
+
+Gym.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    gym_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'gym',
+  }
+);
+
+module.exports = Gym;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require('./User');
+const Gym = require('./Gym');
 
-module.exports = { User };
+User.belongsTo(Gym, {
+    foreignKey: 'gym_id',
+});
+
+Gym.hasMany(User, {
+    foreignKey: 'gym_id',
+});
+
+module.exports = { User, Gym };
