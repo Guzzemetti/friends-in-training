cd50911e6d5a8ae9f4e4fe8203e1765d801f0f94
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-09 14:39:11 -0400

    added resultsRoutes.js and linked to index.js for the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+
+// Main Routes
 const userRoutes = require('./userRoutes');
+const resultsRoutes = require('./resultsRoutes');
+
 
 router.use('/users', userRoutes);
+router.use('/results', resultsRoutes);
+
 
 module.exports = router;
---
+++ controllers/api/resultsRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+
+
+
+
+module.exports = router;
