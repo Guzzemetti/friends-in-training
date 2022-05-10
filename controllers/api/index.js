ffdbae6bdc3a3bbc98566d295893b6e8aaa8453b
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-10 11:34:54 -0400

    Removed gymRoutes file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,9 @@ const router = require('express').Router();
 // Main Routes
 const userRoutes = require('./userRoutes');
 const resultsRoutes = require('./resultsRoutes');
-const gymRoutes = require('./gymRoutes')
 
 router.use('/users', userRoutes);
 router.use('/results', resultsRoutes);
-router.use('/gyms', gymRoutes);
 
 
 module.exports = router;
--- controllers/api/resultsRoutes.js
+++ controllers/api/resultsRoutes.js
@@ -8,8 +8,17 @@ router.get('/',  (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-
-
-
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+    });
+    if (!userData) {
+      res.status(404).json({ message: 'No user with that ID'})
+      return;
+    } res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
