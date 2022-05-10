acdc58a4b787f29a3305f0f2a80bee01b7f993bd
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-10 10:12:31 -0400

    began results page

--- controllers/api/resultsRoutes.js
+++ controllers/api/resultsRoutes.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 
+router.get('/results', (req, res) => {
+    // find all Users as results
+    User.findAll({
+      include: [User],
+    })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));    
+  });
+
+
+
+
+
 
 
 
