701ba0305fe0b5cb7978d2b95bae755fe9d9dbea
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-11 14:27:10 -0400

    Updated profile routes more, tested functionality and removed duplicate code

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,22 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-
-router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll();
-    const users = userData.map((user) => user.get({ plain: true }));
-    // Pass serialized data and session flag into template
-    res.render('userResults', { 
-      users, 
-      // logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
 router.get('/:id', async (req, res) => {
     try {
       // Find the logged in user based on the session ID
