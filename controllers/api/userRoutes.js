68213dacfbfe73cd4c28b772023be7a98510a81b
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-11 22:29:25 -0400

    cleaned up whitespace in the routes files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -37,30 +37,24 @@ router.post("/", async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
     if (!userData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
     const validPassword = await userData.checkPassword(req.body.password);
-
     if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
